5963adac448c497c56b22dff026d87a907260cd8
Yuta Suzuki <g.uc2180@gmail.com>
2024-04-28 03:54:01 +0900

    視聴画面と、動画検索画面の再生数検索実装

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -6,18 +6,39 @@ class VideosController < ApplicationController
   def show; end
 
   def search
-   # results = @youtube.list_searches(:snippet, q: params[:keyword], type: 'video', max_results:4)
-   # @videos = results.items
-   # render :index
+  #  results = @youtube.list_searches(:snippet, q: params[:keyword], type: 'video', max_results:6, order: 'viewCount')
+  #  @videos = results.items.map do |result|
+  #    video_id = result.id.video_id
+  #    statistics = @youtube.list_videos(:statistics, id: video_id).items.first
+  #    view_count = statistics.statistics.view_count
+  #    {
+  #      title: result.snippet.title,
+  #      video_id: video_id,
+  #      view_count: view_count,
+  #      thumbnail_url: result.snippet.thumbnails.high.url
+  #    }
+  #  end
+    render :index
   end
 
 private
 
-  def set_youtube
-   # @youtube = Google::Apis::YoutubeV3::YouTubeService.new
-   # @youtube.key = ENV['YOUTUBE_API_KEY']
-   # recommend_keyword = "サッカー マルシャル FW"
-   # recommend_results = @youtube.list_searches(:snippet, q: recommend_keyword, type: 'video', max_results:4)
-   # @recommend_videos = recommend_results.items
-  end
+def set_youtube
+  #@youtube = Google::Apis::YoutubeV3::YouTubeService.new
+  #@youtube.key = ENV['YOUTUBE_API_KEY']
+  #recommend_keyword = "サッカー スーパープレイ集 ファンタジスタ"
+  #recommend_results = @youtube.list_searches(:snippet, q: recommend_keyword, type: 'video', max_results: 6, order: 'viewCount')
+  #@recommend_videos = recommend_results.items.map do |result|
+  #  video_id = result.id.video_id
+  #  statistics = @youtube.list_videos(:statistics, id: video_id).items.first
+  #  view_count = statistics.statistics.view_count
+  #  {
+  #    title: result.snippet.title,
+  #    video_id: video_id,
+  #    view_count: view_count,
+  #    thumbnail_url: result.snippet.thumbnails.high.url
+  #  }
+  #end
+end
+
 end
