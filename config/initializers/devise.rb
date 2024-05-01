0468e4fb4bc127e2a87f7c1be44d9194cb8552c1
Yuta Suzuki <g.uc2180@gmail.com>
2024-05-01 20:01:19 +0900

    google認証実装

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -310,4 +310,8 @@ Devise.setup do |config|
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
+
+  #omniauth用設定
+  config.omniauth :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']
+  OmniAuth.config.logger = Rails.logger if Rails.env.development? # debug用
 end
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,8 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /app/tmp/pids/server.pid
 
-#bin/rails assets:precompile
+RAILS_ENV=production bin/rails db:migrate
+RAILS_ENV=production bin/rails assets:precompile
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
