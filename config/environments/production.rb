10141aa6ea4b0ae4e95fd55c6cc96aaf9195f18e
Yuta Suzuki <g.uc2180@gmail.com>
2024-05-02 04:37:46 +0900

    4push

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,6 @@ Rails.application.configure do
   
   #device用の設定
   config.action_mailer.default_url_options = { host: 'https://stardom-jonn.onrender.com' }
-  config.action_mailer.delivery_method = :smtp
   
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
