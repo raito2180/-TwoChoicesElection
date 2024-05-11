4a31da687da93e655bea89007cfb414c05570e45
Yuta Suzuki <g.uc2180@gmail.com>
2024-05-12 00:26:06 +0900

    contactにdependentdestroy追加、既存ユーザー削除

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,7 @@
 class Contact < ApplicationRecord
   attr_accessor :email
   belongs_to :user
+  has_many :responses, dependent: :destroy
   with_options presence: true do
     validates :name
     validates :subject
--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,5 @@ module MyApp
     # config.eager_load_paths << Rails.root.join("extras")
     config.time_zone = 'Tokyo'
 		config.active_record.default_timezone = :local
-		config.i18n.default_locale = :ja
   end
 end
