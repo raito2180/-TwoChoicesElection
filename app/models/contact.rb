7f7dc4938658e9386c8fdeb5385ada7c893c8506
Yuta Suzuki <g.uc2180@gmail.com>
2024-05-12 00:45:33 +0900

    contactのdependentdestroy削除

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,7 +1,6 @@
 class Contact < ApplicationRecord
   attr_accessor :email
   belongs_to :user
-  has_many :responses, dependent: :destroy
   with_options presence: true do
     validates :name
     validates :subject
