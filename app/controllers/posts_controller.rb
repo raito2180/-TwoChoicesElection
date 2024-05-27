1437f6622650114b002f20268f3f448051589ec8
Yuta Suzuki <g.uc2180@gmail.com>
2024-05-28 04:57:14 +0900

    マッチング募集一覧画面から自分以外のユーザーのプロフィールを見に行けるようになりました。アイコンのレンダリング調整を行いました

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,28 @@
 class PostsController < ApplicationController
-  def index; end
+
+  def index
+    @posts = Post.all
+  end
 
   def new 
     @post = Post.new
   end
 
   def create
-    
+    @post = current_user.profile.posts.new(post_params)
+    if @post.save
+      redirect_to @post, notice: '募集を投稿しました'
+    else
+      flash.now[:danger] = '募集の投稿に失敗しました'
+      render :new
+    end
   end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :date, :location, :detail, :capacity, :related_url )
+  end
+
 
 end
