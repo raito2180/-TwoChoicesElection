fb6b6cb0ca317490698f48fda6a8c52f3a503e8b
Yuta Suzuki <g.uc2180@gmail.com>
2024-06-09 13:28:18 +0900

    logger.debugを差し込みました

--- app/channels/chatroom_channel.rb
+++ app/channels/chatroom_channel.rb
@@ -14,11 +14,13 @@ class ChatroomChannel < ApplicationCable::Channel
     group = post.group
     if group && profile
       chat = profile.chats.create!(body: data['message'], group: group)
+      Rails.logger.debug "Chat created: #{chat.inspect}"
       ActionCable.server.broadcast('chatroom_channel', {message: render_message(chat), chat_id: chat.id})
     end
   end
 
   def render_message(chat)
+    Rails.logger.debug "Rendering message for chat ID: #{chat.id}, profile ID: #{chat.profile.id}, user ID: #{chat.profile.user.id}"
     ApplicationController.render_with_signed_in_user(
       chat.profile.user, 
       partial: 'chatrooms/chat', 
