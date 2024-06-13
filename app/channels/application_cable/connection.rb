38c7e1d7ad20352e9c2fe612aa33415327f90bb6
Yuta Suzuki <g.uc2180@gmail.com>
2024-06-13 18:14:02 +0900

    認証に失敗しチャットが送信できないためwardenユーザーではなくクッキーからuser_idを取るように変更しました

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -3,13 +3,18 @@ module ApplicationCable
     identified_by :current_user
 
     def connect
-      reject_unauthorized_connection unless find_verified_user
+      self.current_user = find_verified_user
+      logger.add_tags 'ActionCable', current_user.email
     end
 
-    private
+    protected
 
     def find_verified_user
-      self.current_user = env['warden'].user
+      if (verified_user = User.find_by(id: cookies.signed["user.id"]))
+        verified_user
+      else
+        reject_unauthorized_connection
+      end
     end
   end
 end
