49ea225b627b549f2916d66780384fd1ffe91ce5
Yuta Suzuki <g.uc2180@gmail.com>
2024-06-17 20:51:08 +0900

    動画視聴検索時とAI検索時にローディング画面を実装しました

---
+++ app/javascript/controllers/loading_controller.js
@@ -0,0 +1,13 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["modal"]
+
+  show() {
+    this.modalTarget.classList.remove("hidden")
+  }
+
+  hide() {
+    this.modalTarget.classList.add("hidden")
+  }
+}
