c19cdfdb1d29343ab57a661066dd2067cfa2cfbe
Yuta Suzuki <g.uc2180@gmail.com>
2024-06-28 00:57:27 +0900

    membershipのステータス入力・変更時のフラッシュメッセージの表示をdecoratorに切り分けました

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -3,7 +3,7 @@ class MembershipsController < ApplicationController
     group = Group.find(params[:group_id])
     membership = group.memberships.new(profile: current_user.profile, status: params[:status])
     if membership.save
-      redirect_to request.referer, notice: create_notice_message(membership.status)
+      redirect_to request.referer, notice: membership.decorate.create_notice_message
     else
       redirect_to request.referer, alert: '状態の更新に失敗しました。'
     end
@@ -15,36 +15,12 @@ class MembershipsController < ApplicationController
 
     if membership
       if membership.update(status: params[:status])
-        redirect_to request.referer, notice: update_notice_message(membership.status)
+        redirect_to request.referer, notice: membership.decorate.update_notice_message
       else
         redirect_to request.referer, alert: '状態の更新に失敗しました。'
       end
     else
       redirect_to request.referer, alert: 'Membershipが見つかりませんでした。'
-    end
-  end
-
-  private
-
-  def create_notice_message(status)
-    case status
-    when '参加'
-      '参加しました。'
-    when '不参加'
-      '不参加で入力しました。'
-    else
-      '興味ありで入力しました。'
-    end
-  end
-
-  def update_notice_message(status)
-    case status
-    when '参加'
-      '参加に変更しました。'
-    when '不参加'
-      '不参加に変更しました。'
-    else
-      '興味ありに変更しました。'
     end
   end
 
---
+++ app/decorators/membership_decorator.rb
@@ -0,0 +1,26 @@
+class MembershipDecorator < Draper::Decorator
+  delegate_all
+
+  def create_notice_message
+    case object.status
+    when '参加'
+      '参加しました。'
+    when '不参加'
+      '不参加で入力しました。'
+    else
+      '興味ありで入力しました。'
+    end
+  end
+
+  def update_notice_message
+    case object.status
+    when '参加'
+      '参加に変更しました。'
+    when '不参加'
+      '不参加に変更しました。'
+    else
+      '興味ありに変更しました。'
+    end
+  end
+
+end
