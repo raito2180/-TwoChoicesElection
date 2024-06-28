0ab5c46d412fa7d2c742374d4b099b7abdf2f94b
Yuta Suzuki <g.uc2180@gmail.com>
2024-06-29 02:29:45 +0900

    rubocopによるコードの修正を行いました

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
-    email                 {'test@yahoo.com'}
-    password              {'111111'}
-    password_confirmation {'111111'}
+    email                 { 'test@yahoo.com' }
+    password              { '111111' }
+    password_confirmation { '111111' }
   end
 end
