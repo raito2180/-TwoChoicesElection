2cd5ec6639cba2ac0432834f284d5adc4d404a59
Yuta Suzuki <g.uc2180@gmail.com>
2024-07-07 01:20:34 +0900

    自分が参加しているグループチャット一覧を表示するviewを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'groupchats/index'
   mount ActionCable.server => '/cable'
 
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
