8932e646b7307957d6fc3e2763885c1ba687d07d
Yuta Suzuki <g.uc2180@gmail.com>
2024-07-06 23:58:24 +0900

    自分が参加しているグループチャット一覧を表示するgroupchatsアクションを作成

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,8 @@ Rails.application.routes.draw do
 
   resources :profiles, only: [:show, :edit, :update]
 
+  resources :groupchats, only: [:index] 
+
   resources :responses
 
   resources :posts do
